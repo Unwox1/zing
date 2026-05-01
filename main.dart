dd88aa878cfbb40c9972bdef9698ebe1fdef3054
Unwox1 <82282176+Unwox1@users.noreply.github.com>
2026-05-01 22:31:56 +0300

    Update main.dart

--- main.dart
+++ main.dart
@@ -1,1 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:zingapp/core/theme/app_theme.dart';
+import 'package:zingapp/features/map/screens/map_home_screen.dart';
 
+void main() {
+  runApp(const ZingApp());
+}
+
+class ZingApp extends StatelessWidget {
+  const ZingApp({
+    super.key,
+    this.enableSimulation = true,
+  });
+
+  final bool enableSimulation;
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Zing',
+      debugShowCheckedModeBanner: false,
+      theme: AppTheme.light,
+      home: MapHomeScreen(enableSimulation: enableSimulation),
+    );
+  }
+}
